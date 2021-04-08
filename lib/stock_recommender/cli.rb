485d9afd311fda6950eb778aab5799a9c15295d4
rosh-agahi <rosh.agahi@gmail.com>
2021-04-07 20:13:31 -0700

    qa confirmed input loop

--- lib/stock_recommender/cli.rb
+++ lib/stock_recommender/cli.rb
@@ -17,22 +17,22 @@ class StockRecommender::CLI
     puts "Please select a sector (1-21) by typing the number corresponding to it and pressing enter."
     puts "If you would like to exit, type e and press enter."
 
-    # loop do
-    # input = gets.chomp
-    # case input
-    #   when '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12','13', '14', '15', '16', '17', '18', '19', '20'
-    #     i = input.to_i - 1
-    #     Sector.sector_stocks(i)
-    #     puts "\n Select another sector (1-20), show list (r), exit (e)"
-    #   when 'r'
-    #     Sector.all
-    #     puts "\n Select another sector (1-20), show list (r), exit (e)"
-    #   when 'e'
-    #     break
-    #   else
-    #     puts "error message"
-    #   end
-    # end
+    loop do
+    input = gets.chomp
+    case input
+      when '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12','13', '14', '15', '16', '17', '18', '19', '20'
+        i = input.to_i - 1
+        # Sector.sector_stocks(i)
+        puts "\n Select another sector (1-20), show list (r), exit (e)"
+      when 'r'
+        # Sector.all
+        puts "\n Select another sector (1-20), show list (r), exit (e)"
+      when 'e'
+        break
+      else
+        puts "error message"
+      end
+    end
 
   end
 end
