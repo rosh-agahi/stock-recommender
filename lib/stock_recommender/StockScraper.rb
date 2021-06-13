69cacec53b671e700e93a3d0d25547f92e83957b
rosh-agahi <rosh.agahi@gmail.com>
2021-06-13 15:47:48 -0700

    fix company name scraping with upcase

--- lib/stock_recommender/StockScraper.rb
+++ lib/stock_recommender/StockScraper.rb
@@ -10,9 +10,9 @@ class StockScraper
     @stockrows = doc.css("tbody").css("tr")
 
     @stockrows.each do |stock|
-      n=3 #this is used to cut the company name to the first two words available in the description in the span element.
+      n=3 #this is used to cut the company name to the first three words available in the description in the span element.
       ticker = stock.css("td").css("a")[0].text
-      company = stock.css("td").css("span.tv-screener__description").text.strip.gsub(/[^A-Z ]/, '').split(/\s+/, n+1)[0...n].join(' ')
+      company = stock.css("td").css("span.tv-screener__description").text.upcase.strip.gsub(/[^A-Z ]/, '').split(/\s+/, n+1)[0...n].join(' ')
       price = stock.css("td")[1].text
       percentchange = stock.css("td")[2].text
       reco = stock.css("td")[4].text
